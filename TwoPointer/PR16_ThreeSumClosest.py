f87f22b54c675af00a1b2801b278e644ed02c916
Shri Ram dwivedi <159000468+shri-ram07@users.noreply.github.com>
2026-01-02 18:01:46 +0530

    Updating the logic behind the 3SumClosest Problem

--- TwoPointer/PR16_ThreeSumClosest.py
+++ TwoPointer/PR16_ThreeSumClosest.py
@@ -3,7 +3,7 @@ class Solution(object):
         # Sort The array
         # Fix one pointer and use two pointers to find the closest sum
         # We have to return the sum which is closest to target
-        # Closet Sum will have diff 0 so we can break the loop there
+        
         closest_sum = nums[0] + nums[1] + nums[2]
         closest_sum_diff = max(nums)+10**100
 
@@ -12,6 +12,9 @@ class Solution(object):
             print("Loop started")
             x , y = i+1 , len(nums)-1
             while x<y:
+                # Do same as 3 sum to fins the sum equals to target 
+                # if found then return directly and update the two pionters accordingly
+
                 sum = nums[i] + nums[x] + nums[y]
                 print("Sum  is ", sum)
                 if sum == target:
@@ -20,7 +23,11 @@ class Solution(object):
                     x+=1
                 elif sum > target:
                     y-=1
-                    
+                
+                # Main Crux to find the closest sum
+                # Here we will check if the current sum is closer to target than the previous closest sum
+                # If yes then we will update the closest sum
+                
                 closest_sum_diff_current = abs(sum-target)
                 if closest_sum_diff > closest_sum_diff_current:
                     print("Closest Sum is", closest_sum)
